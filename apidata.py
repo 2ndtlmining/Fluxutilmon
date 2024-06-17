58be10e1c0c41efb41d03ce6fed3304274ee5ea1
2nTL <2ndtlmining@gmail.com>
2024-06-17 10:38:34 +1000

    error handling in FluxAPI that doesnt return data all the time

--- apidata.py
+++ apidata.py
@@ -7,12 +7,36 @@ import datetime
 benchurl = "https://stats.runonflux.io/fluxinfo?projection=benchmark"
 utilurl = "https://stats.runonflux.io/fluxinfo?projection=apps.resources"
 totalnodeurl = "https://api.runonflux.io/daemon/getzelnodecount"
+walleturl = "https://api.runonflux.io/daemon/viewdeterministiczelnodelist"
 
 async def fetch_bench_data(url):
-    async with aiohttp.ClientSession() as session:
-        async with session.get(url) as response:
-            data = await response.json()
-            return data
+    try:
+        async with aiohttp.ClientSession() as session:
+            async with session.get(url) as response:
+                if response.status == 200:
+                    data = await response.json()
+                    return data
+                else:
+                    raise Exception(f"Request failed with status code: {response.status}")
+    except aiohttp.ClientError as e:
+        # Handle any other request-related errors
+        print(f"An error occurred: {str(e)}")
+        return None
+        
+
+async def fetch_wallet_data(url):
+    try:
+        async with aiohttp.ClientSession() as session:
+            async with session.get(url) as response:
+                if response.status == 200:
+                    data = await response.json()
+                    return data
+                else:
+                    raise Exception(f"Request failed with status code: {response.status}")
+    except aiohttp.ClientError as e:
+        # Handle any other request-related errors
+        print(f"An error occurred: {str(e)}")
+        return None
         
 async def fetch_util_data(url):
     try:
@@ -42,27 +66,40 @@ async def main():
     bench_data = await fetch_bench_data(benchurl)
     util_data = await fetch_util_data(utilurl)
     total_nodes_data = await fetch_totalnodes(totalnodeurl)
+    wallet_data = await fetch_wallet_data(walleturl)
+
+    try:
+        totalbenchmarkcores = sum(record['benchmark']['bench']['cores'] for record in bench_data['data'])
+        totalbenchmarkram = sum(record['benchmark']['bench']['ram'] for record in bench_data['data'])
+        totalbenchmarkssd = sum(record['benchmark']['bench']['ssd'] for record in bench_data['data'])
+        
+        totalutilcores = sum(record['apps']['resources']['appsCpusLocked'] for record in util_data['data'])
+        totalutilram = ((sum(record['apps']['resources']['appsRamLocked'] for record in util_data['data']))) / 1000
+        totalutilssd = sum(record['apps']['resources']['appsHddLocked'] for record in util_data['data'])
     
-    totalbenchmarkcores = sum(record['benchmark']['bench']['cores'] for record in bench_data['data'])
-    totalbenchmarkram = (sum(record['benchmark']['bench']['ram'] for record in bench_data['data']))
-    totalbenchmarkssd = sum(record['benchmark']['bench']['ssd'] for record in bench_data['data'])
+        notutilizednodes = sum(1 for record in util_data['data'] if record['apps']['resources']['appsRamLocked'] == 0)
     
-    totalutilcores = sum(record['apps']['resources']['appsCpusLocked'] for record in util_data['data'])
-    totalutilram = ((sum(record['apps']['resources']['appsRamLocked'] for record in util_data['data']))) / 1000
-    totalutilssd = sum(record['apps']['resources']['appsHddLocked'] for record in util_data['data'])
+        totalnodes = total_nodes_data['data']['total']
+        total_cumulus = total_nodes_data['data']['cumulus-enabled']
+        total_nimbus = total_nodes_data['data']['nimbus-enabled']
+        total_stratus = total_nodes_data['data']['stratus-enabled']
     
-    notutilizednodes = sum(1 for record in util_data['data'] if record['apps']['resources']['appsRamLocked'] == 0)
+        utilization_percentage_cores = (totalutilcores / totalbenchmarkcores) * 100
+        utilization_percentage_ram = (totalutilram / totalbenchmarkram) * 100
+        utilization_percentage_ssd = (totalutilssd / totalbenchmarkssd) * 100
+        utilization_nodes = (totalnodes - notutilizednodes) / totalnodes * 100
+        snapshot_date = datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
+
+
+     # Unique wallet ids
+        unique_wallet_ids = set(record['payment_address'] for record in wallet_data['data'])
+        unique_wallet_count = len(unique_wallet_ids)  # Calculate the count of unique wallet ids
+        print(f'Unique Wallet IDs: {unique_wallet_count}')
+    except (KeyError, TypeError) as e:
+        print(f"Error processing bench_data: {str(e)}")
+        # Handle the error gracefully, e.g., retry later or continue without creating the JSON file
+
     
-    totalnodes = total_nodes_data['data']['total']
-    total_cumulus = total_nodes_data['data']['cumulus-enabled']
-    total_nimbus = total_nodes_data['data']['nimbus-enabled']
-    total_stratus = total_nodes_data['data']['stratus-enabled']
-    
-    utilization_percentage_cores = (totalutilcores / totalbenchmarkcores) * 100
-    utilization_percentage_ram = (totalutilram / totalbenchmarkram) * 100
-    utilization_percentage_ssd = (totalutilssd / totalbenchmarkssd) * 100
-    utilization_nodes = (totalnodes - notutilizednodes) / totalnodes * 100
-    snapshot_date = datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
 
     jsondata = {
         'Snapshot': snapshot_date,
@@ -80,7 +117,8 @@ async def main():
         'utilization_nodes': utilization_nodes,
         'total_cumulus': total_cumulus,
         'total_nimbus': total_nimbus,
-        'total_stratus': total_stratus
+        'total_stratus': total_stratus,
+        'unique_wallet_count': unique_wallet_count
 
     }
     filename = f'utilization_{snapshot_date}.json'
@@ -110,7 +148,11 @@ async def main():
     print(f'Utilization Percentage (Ram): {utilization_percentage_ram:.2f}%')
     print(f'Utilization Percentage (SSD): {utilization_percentage_ssd:.2f}%')
     print(f'Utilization Nodes: {utilization_nodes:.2f}%')
+    print(f'Unique Wallet IDs: {unique_wallet_count}')
 
 # Create and run the event loop
 loop = asyncio.get_event_loop()
-loop.run_until_complete(main())
+try:
+    loop.run_until_complete(main())
+finally:
+    loop.close()

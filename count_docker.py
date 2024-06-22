99619f68fa06f5c5c9e9997f10a7a6cc72f9c914
2nTL <2ndtlmining@gmail.com>
2024-06-23 05:25:52 +1000

    updated error handling if API doesnt return response with creating json file

--- count_docker.py
+++ count_docker.py
@@ -24,27 +24,27 @@ def get_running_apps():
                             image_counts[image] = 1
                         total_count += 1
 
-        sorted_counts = sorted(image_counts.items(), key=lambda x: x[1], reverse=True)
+        if total_count > 0:
+            current_time = datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
+            file_name = f'data/docker_count_{current_time}.json'
+            os.makedirs(os.path.dirname(file_name), exist_ok=True)
+            existing_data = {}
 
-        current_time = datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
-        file_name = f'data/docker_count_{current_time}.json'
-        os.makedirs(os.path.dirname(file_name), exist_ok=True)
-        existing_data = {}
+            if os.path.exists(file_name):
+                with open(file_name, 'r') as f:
+                    existing_data = json.load(f)
+            existing_data["Snapshot"] = current_time
+            existing_data["Total Docker Count"] = total_count
+            existing_data["ImageCounts"] = image_counts
+            with open(file_name, 'w') as f:
+                json.dump(existing_data, f, indent=4)
 
-        if os.path.exists(file_name):
-            with open(file_name, 'r') as f:
-                existing_data = json.load(f)
-        existing_data["Snapshot"] = current_time
-        existing_data["Total Docker Count"] = total_count
-        existing_data["ImageCounts"] = image_counts
-        with open(file_name, 'w') as f:
-            json.dump(existing_data, f, indent=4)
-
-        print(f'Data written to {file_name}')
-        print(f"Total running apps: {total_count}")
-        for i, (image, count) in enumerate(sorted_counts, start=1):
-            print(f"{i}. {image}: {count}")
+            print(f'Data written to {file_name}')
+            print(f"Total running apps: {total_count}")
+            for i, (image, count) in enumerate(sorted(image_counts.items(), key=lambda x: x[1], reverse=True), start=1):
+                print(f"{i}. {image}: {count}")
 
     except requests.exceptions.RequestException as e:
         print(f"An error occurred while making the request: {str(e)}")
+        
 get_running_apps()
